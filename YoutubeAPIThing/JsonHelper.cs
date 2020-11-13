798d29e2bf676c129c05176282bd088347e52a4d
EnBi81 <wotng2002@gmail.com>
2020-11-13 23:23:42 +0100

    Add files via upload

---
+++ YoutubeAPIThing/JsonHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutubeAPIThing.Json
+{
+#pragma warning disable IDE1006
+    internal class JsonHelper
+    {
+        public DidYouMeanRenderer didYouMeanRenderer { get; set; }
+        public PlaylistRenderer playlistRenderer { get; set; }
+
+
+        public bool isPlaylist { get => playlistRenderer is not null && didYouMeanRenderer is null; }
+    }
+
+    #region DidYouMean
+    internal class DidYouMeanRenderer
+    {
+        public DidYouMean didYouMean { get; set; }
+    }
+    
+    internal class DidYouMean
+    {
+        public List<TextJSON> runs { get; set; }
+    }
+    
+    internal class TextJSON
+    {
+        public string text { get; set; }
+    }
+    #endregion
+
+
+
+    internal class PlaylistRenderer
+    {
+        public string playlistId { get; set; }
+        public Title title { get; set; }
+        public string videoCount { get; set; }
+
+        public static implicit operator YTPlaylist(PlaylistRenderer p)
+           => new YTPlaylist(p.playlistId, p.title.simpleText, Convert.ToInt32(p.videoCount));
+    }
+    internal class Title
+    {
+        public string simpleText { get; set; }
+    }
+#pragma warning restore IDE1006
+}
